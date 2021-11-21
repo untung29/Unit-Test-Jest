b69a3418507080b6e94f6dafe6fba2bc76b86dea
Untung <u.untung0029@gmail.com>
2021-11-21 15:28:43 +0700

    Mock/spies

--- async-test.test.js
+++ async-test.test.js
@@ -9,3 +9,19 @@ it("fetching star wards people", () => {
     expect(data.results.length).toBeGreaterThan(5);
   });
 });
+
+it("get people returns count and results", () => {
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () => Promise.resolve({ count: 87, results: [0, 1, 2, 3, 4, 5] }),
+    }),
+  );
+  expect.assertions(4);
+
+  return swapi(mockFetch).then(data => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith("https://swapi.py4e.com/api/people/");
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
